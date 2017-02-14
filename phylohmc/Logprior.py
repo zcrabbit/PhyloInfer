d79416803b2b2fe794903e799f24ea3877a49f49
Cheng Zhang <zc.rabbit@gmail.com>
2017-02-13 16:33:52 -0800

    Remove old version

--- phylohmc/Logprior.py
+++
@@ -1,9 +0,0 @@
-
-# Logprior and its gradient
-import numpy as np
-
-def phyloLogprior_exp(branch, scale=0.1, grad=False):
-    if not grad:
-        return -np.sum(branch)/scale - np.log(scale)*len(branch)
-    else:
-        return -np.ones(len(branch))/scale
