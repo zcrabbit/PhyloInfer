a244cc801fea0432054b6fb79edb61f47d106297
Cheng Zhang <zc.rabbit@gmail.com>
2017-02-13 17:07:08 -0800

    Branch length manipulation

---
+++ phyloinfer/branchManipulation.py
@@ -0,0 +1,19 @@
+
+# tree branch length manipulations
+
+def update(tree, dbranch):
+    for node in tree.traverse("postorder"):
+        if not node.is_root():
+            node.dist += dbranch[node.name]
+
+def get(tree):
+    branch = [0] * (2*len(tree)-3)
+    for node in tree.traverse("postorder"):
+        if not node.is_root():
+            branch[node.name] = node.dist
+    return branch
+
+def set(tree,branch):
+    for node in tree.traverse("postorder"):
+        if not node.is_root():
+            node.dist = branch[node.name]
