4235f453cc34a2a6d0d2106cdeab7a02a56d41a4
Cheng Zhang <zc.rabbit@gmail.com>
2017-07-15 14:58:02 -0700

    Add create function

--- phyloinfer/treeManipulation.py
+++ phyloinfer/treeManipulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from phyloinfer import Tree
 
 
 
@@ -15,9 +16,9 @@ def init(tree, branch=None, name='all', scale=0.1, display=False, return_map=Fal
         else:
             node.name, j = j, j+1
         if not node.is_root():
-            if branch=='random':
+            if isinstance(branch, basestring) and branch=='random':
                 node.dist = np.random.exponential(scale)
-            elif branch:
+            elif branch is not None:
                 node.dist = branch[node.name]
         else:
             node.dist = 0.0
@@ -27,6 +28,15 @@ def init(tree, branch=None, name='all', scale=0.1, display=False, return_map=Fal
             print node.name, node.dist
         
     if return_map: return idx2node
+    
+    
+def create(ntips, branch='random', scale=0.1):
+    tree = Tree()
+    tree.populate(ntips)
+    tree.unroot()
+    init(tree, branch=branch, scale=scale)
+    
+    return tree
     
 
 def namenum(tree, taxon):
